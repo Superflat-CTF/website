68421de4963b792a9e5b66d77c0955813403ab8d
woutvanderploeg <supervrijdag@protonmail.com>
2024-11-15 17:35:22 +0100

    nextjs init

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Superflat",
+  description: "The CTF team Superflat",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+
+    <head>
+        <link rel="icon" href="superflat-logo-round.svg"/>
+    </head>
+
+    <body
+        className={` antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
